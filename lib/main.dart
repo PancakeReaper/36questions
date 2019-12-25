5b7d2118e8b70d8a63654e612bd7d952ab255d16
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-12-25 15:31:57 -0800

    Refactored background & question widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:questions_36/background.dart';
+import 'package:questions_36/question_text.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,45 +21,9 @@ class MyApp extends StatelessWidget {
         body: Stack(
           children: [
             // Container for the background number
-            Container(
-              constraints: BoxConstraints.expand(
-                  width: double.infinity, height: double.infinity),
-              color: Colors.red[800],
-              child: Center(
-                  child: Text("36",
-                      style: TextStyle(
-                        fontFamily: 'Lobster',
-                        fontSize: 220,
-                        color: Colors.white12,
-                        shadows: [
-                          Shadow(
-                            color: Colors.white24,
-                            blurRadius: 5.0,
-                          ),
-                        ],
-                      ))),
-            ),
+            Background(),
             // Container for the question
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 100.0, vertical: 25.0),
-              child: Center(
-                child: Text(
-                  "Some long ass question text and more personal attacks?",
-                  style: TextStyle(
-                    fontFamily: 'DancingScript',
-                    fontSize: 56,
-                    color: Colors.grey[300],
-                    shadows: [
-                      Shadow(
-                          color: Colors.grey[800],
-                          offset: Offset(3.5, 3.5),
-                          blurRadius: 10.0),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
+            QuestionText(),
           ],
         ),
       ),
