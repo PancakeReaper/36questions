62eb6364d9a129f4763c8c1d04fd3c5b3d6d45b4
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-12-24 23:31:09 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
+  ]);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Container(
+        constraints: BoxConstraints.expand(
+            width: double.infinity, height: double.infinity),
+        color: Colors.redAccent,
+      )),
+    );
+  }
+}
