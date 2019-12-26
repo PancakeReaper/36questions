cd61a872a48a956f7513f46ac688209fa09d57ae
PancakeReaper <25124648+PancakeReaper@users.noreply.github.com>
2019-12-25 18:06:44 -0800

    fixed font to fit questions

--- lib/style.dart
+++ lib/style.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 // I kinda want this as const but Colors.grey[800] is not a const value?
 final Body2TextStyle = TextStyle(
   fontFamily: 'DancingScript',
-  fontSize: 56,
+  fontSize: 40,
   color: Colors.grey[300],
   shadows: [
     Shadow(
